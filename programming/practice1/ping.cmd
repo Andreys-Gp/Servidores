03ecd0d96e97d7103fa6657fa2a7b488213206f8
Brayan Dev <andreys6gonzalez@gmail.com>
2024-10-11 18:01:15 -0500

    avances

--- programming/practice1/ping.cmd
+++ programming/practice1/ping.cmd
@@ -12,3 +12,44 @@ Ping statistics for 192.168.0.1:
     Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
 Approximate round trip times in milli-seconds:
     Minimum = 0ms, Maximum = 35ms, Average = 8ms
+
+
+
+
+    ------------------------------------------------------------------
+
+    
+Cisco Packet Tracer PC Command Line 1.0
+C:\>ping 192.168.0.1
+
+Pinging 192.168.0.1 with 32 bytes of data:
+
+Reply from 192.168.0.1: bytes=32 time=35ms TTL=255
+Reply from 192.168.0.1: bytes=32 time<1ms TTL=255
+Reply from 192.168.0.1: bytes=32 time<1ms TTL=255
+Reply from 192.168.0.1: bytes=32 time<1ms TTL=255
+
+Ping statistics for 192.168.0.1:
+    Packets: Sent = 4, Received = 4, Lost = 0 (0% loss),
+Approximate round trip times in milli-seconds:
+    Minimum = 0ms, Maximum = 35ms, Average = 8ms
+
+C:\>
+C:\>
+C:\>
+C:\>ping  192.168.1.2
+
+Pinging 192.168.1.2 with 32 bytes of data:
+
+Reply from 192.168.0.1: Destination host unreachable.
+Reply from 192.168.0.1: Destination host unreachable.
+Reply from 192.168.0.1: Destination host unreachable.
+Reply from 192.168.0.1: Destination host unreachable.
+
+Ping statistics for 192.168.1.2:
+    Packets: Sent = 4, Received = 0, Lost = 4 (100% loss),
+
+C:\>ping 192.168.1.2
+
+Pinging 192.168.1.2 with 32 bytes of data:
+
